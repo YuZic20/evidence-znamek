0d355953c536ca75c91ab79a836c6d95f39a0abd
Martin Hynek <W0olfikCZ@seznam.cz>
2019-01-31 15:26:37 +0100

    ufff

--- ClassLibrary1/ZnamkaHndle.cs
+++ ClassLibrary1/ZnamkaHndle.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    class ZnamkaHndle
+    public class ZnamkaHndle
     {
-        public
+        public static async Task PridatUpravitZnamku(Znamky znamka)
+        {
+
+            await Tabulka.Database.SaveItemAsync(znamka);
+
+        }
+        public static async Task<Znamky> ZiskatZnamku(int znamka)
+        {
+
+            Znamky getznamka;
+
+            getznamka = await Tabulka.Database.GetItemAsync(znamka);
+
+            return getznamka;
+
+        }
+        public static async Task<List<Znamky>> ZiskatZnamky()
+        {
+
+            List<Znamky> getznamka;
+
+            getznamka = await Tabulka.Database.GetItemsAsync();
+
+            return getznamka;
+
+        }
+        public static async Task SmazatZnamku(Znamky znamka)
+        {
+            await Tabulka.Database.DeleteItemAsync(znamka);
+
+        }
     }
 }
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,36 +1,31 @@
 ﻿using ClassLibrary1;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+
+
             
-
-            x();
-            Console.ReadLine();
-            
-        }
-
-
-        public static async void x()
-        {
-            ClassLibrary1.Tabulka databaze = new ClassLibrary1.Tabulka();
 
             Znamky znamka = new Znamky();
             znamka.IdPredmet = 1;
             znamka.Vaha = 20;
             znamka.Znamka = 1;
 
-            await Tabulka.Database.SaveItemAsync(znamka);
-
-            Znamky getznamka ;
-
-            getznamka = await Tabulka.Database.GetItemAsync(0);
-            int x = 5;
+          
+            
+            Console.ReadLine();
+            
         }
+
+
+        
     }
 }
